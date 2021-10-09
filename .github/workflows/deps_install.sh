7c9640eb405833b786589de99a9001adf7cad838
awalsh128 <awalsh128@gomail.com>
2021-10-08 22:17:50 -0700

    Reformat deps_install.sh

--- .github/workflows/deps_install.sh
+++ .github/workflows/deps_install.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-cache_dir=$1
-packages="dia doxygen doxygen-doc doxygen-gui doxygen-latex graphviz mscgen"
+CACHE_DIR=$1
+PACKAGES="dia doxygen doxygen-doc doxygen-gui doxygen-latex graphviz mscgen"
 
-for dir in `ls $cache_dir`; do
-  remove=true
-  for package in $packages; do
+for dir in `ls $CACHE_DIR`; do
+  REMOVE=true
+  for package in $PACKAGES; do
     if [ $dir = $package ]; then
-      remove=false
+      REMOVE=false
       break
     fi
   done
-  [ "$remove" = true ] && sudo apt remove $dir
+  [ "$REMOVE" = true ] && sudo apt REMOVE $dir
 done
 
-for package in $packages; do
- 
-  package_dir=$cache_dir/$package
-
-  if [ -d $package_dir ]; then
+for package in $PACKAGES; do
   
-    echo "Restoring $package from cache $package_dir..."    
+  package_dir=$CACHE_DIR/$package
+  
+  if [ -d $package_dir ]; then
+    
+    echo "Restoring $package from cache $package_dir..."
     sudo cp --verbose --force --recursive $package_dir/* /
     sudp apt-get -y update $package
   else
-
+    
     echo "Clean install $package and caching to $package_dir..."
     sudo apt-get -y install $package
-
+    
     echo "Caching $package to $package_dir..."
     mkdir -p $package_dir
     # Pipe all package files (no folders) to copy command.
-    sudo dpkg -L $package | 
-      while IFS= read -r f; do 
-        if test -f $f; then echo $f; fi;
-      done | 
-      xargs cp -p -t $package_dir
+    sudo dpkg -L $package |
+    while IFS= read -r f; do
+      if test -f $f; then echo $f; fi;
+    done |
+    xargs cp -p -t $package_dir
   fi
-
+  
 done
