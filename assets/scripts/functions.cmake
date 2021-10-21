8e5020025d528cb2ed2a3d0f859276d0cfd1ef75
awalsh128 <awalsh128@gomail.com>
2021-10-20 20:45:36 -0700

    CMake library for Python builds.

--- assets/scripts/functions.cmake
+++ assets/scripts/functions.cmake
@@ -2,20 +2,27 @@ function(BIN_EXISTS NAME EXIT_CODE)
   execute_process(COMMAND which ${NAME} RESULT_VARIABLE EXIT_CODE)
 endfunction()
 
-function(PY_COMPILE FILENAME)
+function(PY3_BUILD FILENAME)
+  find_program(PY3C python3 "Python 3 compiler executable.")
+  if(NOT PY3C)
+    message(FATAL_ERROR "Python 3 compiler not found.")
+  endif()
+
   file(COPY ${FILENAME}
     DESTINATION ${CMAKE_CURRENT_BINARY_DIR} 
-    USE_SOURCE_PERMISSIONS)
+    USE_SOURCE_PERMISSIONS)  
 
   execute_process(
-    COMMAND python3 -m py_compile ${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}
+    COMMAND ${PY3C} -m py_compile ${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    RESULT_VARIABLE PY_COMPILE_RC)
+    RESULT_VARIABLE PY3_BUILD_RC)
   
-  if (NOT PY_COMPILE_RC EQUAL 0)
+  if (NOT PY3_BUILD_RC EQUAL 0)
     message(FATAL_ERROR 
-      "Python compile of ${FILENAME} exited with return code "
-      "${PY_COMPILE_RC}.")
+      "Python 3 compile of ${FILENAME} exited with return code "
+      "${PY3_BUILD_RC}.")
+  else()
+    message(STATUS "Built Python file ${FILENAME}")
   endif()
 endfunction()
   
