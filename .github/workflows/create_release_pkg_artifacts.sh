373db03d224849bc3a9734bfa8ab899c2280ca1e
awalsh128 <awalsh128@gomail.com>
2021-10-24 13:25:50 -0700

    Experiment with release action.

--- .github/workflows/create_release_pkg_artifacts.sh
+++ .github/workflows/create_release_pkg_artifacts.sh
@@ -9,19 +9,19 @@ cd $PROJECT_DIR
 rm -fr release 2> /dev/null
 mkdir release
 
-RELEASE_PKG_DIR="release/pkg"
+RELEASE_PKG_DIR="release"
 mkdir $RELEASE_PKG_DIR
 
 cat CMakeLists.txt | sed -n '/^# HEADER-BEGIN/,${p;/^# HEADER-END/q}' > $RELEASE_PKG_DIR/CMakeLists.txt;
-cp -f src/*.cpp release/pkg
-cp -f src/*.h release/pkg
-cp -f assets/scripts/install.sh release/pkg
+cp -f src/*.cpp release
+cp -f src/*.h release
+cp -f assets/scripts/install.sh release
 
-cd release/pkg
+cd release
 PKG_NAME="fluentcpp-$TAG"
 tar -czvf $PKG_NAME.tar.gz *
 zip $PKG_NAME.zip *
 
 cd ..
-echo "**Full Changelog**: https://github.com/awalsh128/fluent/commits/$TAG" > notes.md
+echo "**Full Changelog**: https://github.com/awalsh128/fluentcpp/commits/$TAG" > notes.md
 echo $NOTES >> notes.md
