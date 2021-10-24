60f3694a1dd966b3e3e790fe8793ab3ba03651df
awalsh128 <awalsh128@gomail.com>
2021-10-24 14:15:35 -0700

    Debug and fix release creation script.

--- .github/workflows/create_release_pkg_artifacts.sh
+++ .github/workflows/create_release_pkg_artifacts.sh
@@ -8,20 +8,23 @@ cd $PROJECT_DIR
 
 rm -fr release 2> /dev/null
 mkdir release
+mkdir release/src
 
-RELEASE_PKG_DIR="release/pkg"
-mkdir $RELEASE_PKG_DIR
+cat CMakeLists.txt \
+| sed -n '/^# HEADER-BEGIN/,${p;/^# HEADER-END/q}' \
+> release/src/CMakeLists.txt;
 
-cat CMakeLists.txt | sed -n '/^# HEADER-BEGIN/,${p;/^# HEADER-END/q}' > $RELEASE_PKG_DIR/CMakeLists.txt;
-cp -f src/*.cpp release/pkg
-cp -f src/*.h release/pkg
-cp -f assets/scripts/install.sh release/pkg
+cat src/CMakeLists.txt > release/src/CMakeLists.txt
 
-cd release/pkg
+cp -f src/*.cpp release/src
+cp -f src/*.h release/src
+cp -f assets/scripts/install.sh release/src
+
+echo "**Full Changelog**: https://github.com/awalsh128/fluentcpp/releases/tag/$TAG\n\n" > release/notes.md
+echo $NOTES >> release/notes.md
+
 PKG_NAME="fluentcpp-$TAG"
-tar -czvf $PKG_NAME.tar.gz *
-zip $PKG_NAME.zip *
 
-cd ..
-echo "**Full Changelog**: https://github.com/awalsh128/fluent/commits/$TAG" > notes.md
-echo $NOTES >> notes.md
+tar -czvf release/$PKG_NAME.tar.gz -C release/src .
+zip -j release/$PKG_NAME.zip release/src/*
+rm -fr release/src
